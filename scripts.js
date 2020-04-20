8246f89fae7d055d773128fc6b56c74c24f42fad
Lima <rejaine.limafarias@qantas.com.au>
2020-04-20 14:47:05 +1000

    refactor settimeout inside cleartimeout

--- scripts.js
+++ scripts.js
@@ -2,10 +2,8 @@ const filter = async(value) =>
    fetch(`https://jsonplaceholder.typicode.com/users?name_like=${value}`)
     .then(res => res.json())
 
-const debounceEvent = (fn, wait = 1000, time) => (...args) => {
-    clearTimeout(time)
-    time = setTimeout(() => fn(...args), wait)
-}
+const debounceEvent = (fn, wait = 1000, time) => (...args) =>
+  clearTimeout(time, time = setTimeout(() => fn(...args), wait))
 
 function handleKeyUp(event) {
   filter(event.target.value)
