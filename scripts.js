156cc1845b4f0bcf0cb39c271ef18a4fb45f6567
Lima <rejaine.limafarias@qantas.com.au>
2020-04-20 14:30:46 +1000

    refactor debounce to return function inside arrow function and send ...args

--- scripts.js
+++ scripts.js
@@ -2,14 +2,9 @@ const filter = async(value) =>
    fetch(`https://jsonplaceholder.typicode.com/users?name_like=${value}`)
     .then(res => res.json())
 
-const debounceEvent = (fn, wait = 1000, time) => {
-  return function() {
+const debounceEvent = (fn, wait = 1000, time) => (...args) => {
     clearTimeout(time)
-
-    time = setTimeout(() => {
-      fn.apply(this, arguments)
-    }, wait)
-  }
+    time = setTimeout(() => fn(...args), wait)
 }
 
 function handleKeyUp(event) {
