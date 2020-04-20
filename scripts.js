e605b70b9ffb741818e49d8c77b59869a9c23ad1
Lima <rejaine.limafarias@qantas.com.au>
2020-04-20 12:25:55 +1000

    call filter promise inside handleKeyp function

--- scripts.js
+++ scripts.js
@@ -3,15 +3,13 @@ let time = null
 const filter = async(value) =>
    fetch(`https://jsonplaceholder.typicode.com/users?name_like=${value}`)
     .then(res => res.json())
-    .then(data => console.log(data))
-
-filter('in')
 
 function handleKeyUp(event) {
   clearTimeout(time)
 
   time = setTimeout(() => {
-    console.log(event.target.value)
+    filter(event.target.value)
+      .then(data => console.log(data))
   }, 1000);
 }
 
